a812028d9909f2e31c456c5262d23fa9d548edee
kjh3165 <jhwk109@gmail.com>
2026-03-17 23:03:39 +0900

    3

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
+import { useState } from "react";
+
 function App() {
+  const [todos, setTodos] = useState(["할일1", "할일2", "할일3"]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    setTodos([...todos, form.todo.value]);
+    form.todo.value = "";
+  };
+
   return (
     <>
-      <input type="text" placeholder="할일을 입력하세요" />
-      <button>추가</button>
+      <form onSubmit={handleSubmit}>
+        <input type="text" id="todo" placeholder="할일을 입력하세요" />
+        <button type="submit">추가</button>
+      </form>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
     </>
   );
 }
