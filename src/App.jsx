a025d9568f19116bdcf80694a93ee8e9037a9e4d
kjh3165 <jhwk109@gmail.com>
2026-03-17 17:25:31 +0900

    2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 function App() {
-  return <>Todo Tutorial</>;
+  return (
+    <>
+      <input type="text" placeholder="할일을 입력하세요" />
+      <button>추가</button>
+    </>
+  );
 }
 
 export default App;
