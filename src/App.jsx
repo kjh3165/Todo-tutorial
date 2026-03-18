e19cfe43736884a0262f3a3df90fe52464925684
kjh3165 <jhwk109@gmail.com>
2026-03-18 17:22:18 +0900

    12

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,9 @@
-import { useRef, useState } from "react";
 import TodoList from "./TodoList";
 import TodoWriteForm from "./TodoWriteForm";
+import useTodos from "./useTodos";
 
 function App() {
-  const [todos, setTodos] = useState([
-    { id: 1, text: "할일1", checked: true },
-    { id: 2, text: "할일2", checked: false },
-    { id: 3, text: "할일3", checked: true },
-  ]);
-
-  const lastId = useRef(4);
-
-  const addTodo = (text) => {
-    setTodos([...todos, { id: lastId.current++, text: text, checked: false }]);
-  };
-
-  const removeTodo = (id) => {
-    setTodos(todos.filter((todo) => todo.id != id));
-  };
-
-  const toggleTodo = (id) => {
-    setTodos(
-      todos.map((todo) =>
-        todo.id != id ? todo : { ...todo, checked: !todo.checked },
-      ),
-    );
-  };
+  const { todos, addTodo, removeTodo, toggleTodo } = useTodos();
 
   return (
     <>
